14d7ad51e05e3eb09dd881cfd2b36eca54a3c670
Matvii Kovtun <matvii.kovtun@eleks.com>
2018-12-26 17:26:15 +0200

    changed colors added  category

--- app.py
+++ app.py
@@ -11,7 +11,22 @@ app = Dash(__name__)
 
 filename = "music.csv"
 df = select_specifics(filename)
-df = df.loc[~(df == 0).any(axis=1)]
+
+vals = ("latin jazz", "hip hop", "blues-rock", "ccm")
+alpha = 0.2
+fill_colors = ("rgba(238, 82, 83, %s)" % alpha,
+               "rgba(52, 31, 151, %s)" % alpha,
+               "rgba(131, 149, 167,%s)" % alpha,
+               "rgba(99, 110, 114, %s)" % alpha)
+
+alpha *= 4
+line_colors = ("rgba(238, 82, 83, %s)" % alpha,
+               "rgba(52, 31, 151, %s)" % alpha,
+               "rgba(131, 149, 167,%s)" % alpha,
+               "rgba(99, 110, 114, %s)" % alpha)
+
+
+# df = df.loc[~(df == 0).any(axis=1)]
 # df = df.dropna() # if want to use 'hottest'
 
 
@@ -26,7 +41,7 @@ def bin_data(ddf):
     data = []
     bins = [i for i in range(40, 231, 10)]
     labels = [str(i) for i in bins[:-1]]
-    for term in ddf["terms"].unique():
+    for term in vals:
         cond = select_by_vals([term], ddf)
         binned = pd.cut(ddf[cond]['tempo'], bins=bins, labels=labels)
         data.append(ddf[cond].groupby(binned).size())
@@ -35,13 +50,10 @@ def bin_data(ddf):
 
 
 def generate_graph():
-    vals = ["latin jazz", "hip hop", "blues-rock"]
-
     cond = select_by_vals(vals, df)
 
     ddf = df[cond]
     ddf = ddf.sort_values("tempo", ascending=True)
-
 
     binned = bin_data(ddf)
 
@@ -50,9 +62,11 @@ def generate_graph():
             go.Scatter(
                 x=list(binned[t].keys()),
                 y=list(binned[t]),
-                name=ddf['terms'].unique()[t],
+                name=vals[t],
                 fill='tozeroy',
-                mode='lines'
+                mode='lines',
+                line=dict(color=line_colors[t]),
+                fillcolor=fill_colors[t]
             ) for t in range(len(binned))
 
         ],
