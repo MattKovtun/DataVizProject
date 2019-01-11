a83e3dd6f28197c433bff259a774474d1c05e31c
Matvii Kovtun <matvii.kovtun@eleks.com>
2019-01-11 15:47:56 +0200

    added faceting, styled graphics

--- twenty_one_pilots/config.py
+++ twenty_one_pilots/config.py
@@ -1,8 +1,8 @@
 line_alpha = .7
-line_colors = ("rgba(131, 149, 167,%s)" % line_alpha,
-               "rgba(53, 59, 72, %s)" % line_alpha,
-               "rgba(225, 177, 44, %s)" % line_alpha)
+line_colors = {"Vessel": "rgba(131, 149, 167, .7)",
+               "Blurryface": "rgba(53, 59, 72, .7)",
+               "Trench": "rgba(225, 177, 44, .7)"}
 
-text_colors = ("rgb(131, 149, 167)",
-               "rgb(53, 59, 72)",
-               "rgb(225, 177, 44)")
+text_colors = {"Vessel": "rgb(131, 149, 167)",
+               "Blurryface": "rgb(53, 59, 72)",
+               "Trench": "rgb(225, 177, 44)"}
--- twenty_one_pilots/main.py
+++ twenty_one_pilots/main.py
@@ -11,52 +11,83 @@ df = pd.read_csv(file, skipinitialspace=True)
 
 app = Dash(__name__)
 
-top_songs = {"Trench": ["Jumpsuit", "My Blood", "Neon Gravestones"],
+top_songs = {"Trench": ["Jumpsuit", "My Blood", "Levitate", "Bandito", "Smithereens", "Cut My Lip"],
              "Vessel": ["Migraine", "Car Radio", "Guns for Hands"],
-             "Blurryface": ["Heavydirtysoul", "Stressed Out", "Tear in My Heart"]}
+             "Blurryface": ["Heavydirtysoul", "Stressed Out", "Fairly Local", "Message Man", "Lane Boy",
+                            "We Don't Believe What's on TV"]}
+
+songs_positions = {"Trench": {"Jumpsuit": "bottom center", "My Blood": "bottom center", "Levitate": "top center",
+                              "Bandito": "top center", "Smithereens": "bottom center", "Cut My Lip": "bottom center"},
+                   "Blurryface": {"Heavydirtysoul": "top center", "Stressed Out": "bottom center",
+                                  "Fairly Local": "top center", "Message Man": "bottom center",
+                                  "Lane Boy": "top center", "We Don't Believe What's on TV": "top center"},
+                   "Vessel": {}}
+
+
+def create_scatter(album):
+    return go.Scatter(
+        x=df["Index"][df["Album"] == album],
+        y=df["BPM"][df["Album"] == album],
+        mode='lines+markers',
+        line=dict(color=line_colors[album]),
+        text=df["Song"][df["Album"] == album],
+        name=album,
+        hoverinfo='y+text'
+
+    )
+
+
+def create_texts(album, all=False):
+    return go.Scatter(
+        x=df["Index"][df["Album"] == album],
+        y=df["BPM"][df["Album"] == album],
+        mode='markers+text',
+        line=dict(color=line_colors[album]),
+        text=[song if song in top_songs[album] or all else None for song in df["Song"][df["Album"] == album]],
+        textposition=[songs_positions[album][song] if song in songs_positions[album] else "top center" for song in
+                      df["Song"][df["Album"] == album]],
+        hoverinfo='none',
+        showlegend=False,
+        textfont=dict(
+            family="Open Sans",
+            size=15,
+            color=text_colors[album]
+        )
+    )
 
 
 def generate_graph():
-    data = [
-        go.Scatter(
-            x=df["Index"][df["Album"] == album],
-            y=df["BPM"][df["Album"] == album],
-            mode='lines+markers',
-            line=dict(color=line_colors[i]),
-            text=df["Song"][df["Album"] == album],
-            name=album,
-            hoverinfo='y+text'
-
-        ) for i, album in enumerate(df["Album"].unique())
-    ]
-    [data.append(
-        go.Scatter(
-            x=df["Index"][df["Album"] == album],
-            y=df["BPM"][df["Album"] == album],
-            mode='markers+text',
-            line=dict(color=line_colors[i]),
-            text=[song if song in top_songs[album] else None for song in df["Song"][df["Album"] == album]],
-            textposition="top center",
-            hoverinfo='none',
-            showlegend=False,
-            textfont=dict(
-                family="Open Sans",
-                size=15,
-                color=text_colors[i]
-            )
-        )
-    ) for i, album in enumerate(df["Album"].unique())]
+    data = [create_scatter(album) for i, album in enumerate(df["Album"].unique()[1:])]
+    [data.append(create_texts(album)) for i, album in enumerate(df["Album"].unique()[1:])]
 
     return {'data': data,
             'layout': go.Layout(
                 yaxis=dict(range=[65, 185]),
-                title='Розподіл останніх альбомів "twenty one pilots" за темпами'
+                title='Розподіл останніх альбомів "twenty one pilots" за темпами',
+                xaxis=dict(showline=False, showgrid=False, showticklabels=False)
+            )}
+
+
+def generate_secondary_graph():
+    data = [create_scatter(df["Album"].unique()[0])]
+    data.append(create_texts(df["Album"].unique()[0], True))
+    return {'data': data,
+            'layout': go.Layout(
+                yaxis=dict(range=[65, 185]),
+                title='',
+                xaxis=dict(showline=False, showgrid=False, showticklabels=False)
+
             )}
 
 
 app.layout = html.Div([
     dcc.Graph(id='graph',
               figure=generate_graph(),
+              config={
+                  'displayModeBar': False
+              }),
+    dcc.Graph(id='secondary-graph',
+              figure=generate_secondary_graph(),
               config={
                   'displayModeBar': False
               })
